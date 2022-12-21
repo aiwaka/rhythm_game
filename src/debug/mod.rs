5ff82424d07e523e53543eae3355188ae2d49ba7
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-12-22 00:47:11 +0900

    move debug function into plugin

--- src/debug/mod.rs
+++ src/debug/mod.rs
@@ -1,4 +1,5 @@
 mod receptor_info;
+mod result;
 
 use bevy::prelude::*;
 
@@ -6,5 +7,6 @@ pub struct AppDebugPlugin;
 impl Plugin for AppDebugPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(self::receptor_info::ReceptorInfoPlugin);
+        app.add_plugin(self::result::DebugResultPlugin);
     }
 }
