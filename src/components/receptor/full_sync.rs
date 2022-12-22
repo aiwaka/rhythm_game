68245ca9d382ffa091c275cd8b81f82be7f386a0
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-12-22 11:04:00 +0900

    fix full_sync receptor

--- src/components/receptor/full_sync.rs
+++ src/components/receptor/full_sync.rs
@@ -3,6 +3,10 @@ use bevy::prelude::*;
 use super::{NotesPattern, PatternReceptor};
 use crate::{events::CatchNoteEvent, resources::note::NoteType};
 
+/// 鍵盤にノーツを収めるにはbool配列を使うが, usizeのインデックスと鍵盤番号i32の対応を行う必要がある.
+/// そのため鍵盤番号が連番なのを前提として最小の番号がどれになるかを指定する.
+const KEY_FIRST_NUM: i32 = 0;
+
 /// 4点同時押し
 #[derive(Component)]
 pub struct FullSyncReceptor {
@@ -36,8 +40,9 @@ impl PatternReceptor for FullSyncReceptor {
         self.lane.iter().all(|&e| !e)
     }
 
-    fn initialize_or_defer(&mut self, current_time: f64, _: f32) {
-        if (current_time - self.first_time).abs() > 0.1 {
+    fn initialize_or_defer(&mut self, current_time: f64, bpm: f32) {
+        // 最初の受付から1/6拍の時間経過でリセット
+        if (current_time - self.first_time).abs() > 10.0 / bpm as f64 {
             self.init();
         }
     }
@@ -45,13 +50,12 @@ impl PatternReceptor for FullSyncReceptor {
     fn input(&mut self, note_ev: &CatchNoteEvent) {
         if let NoteType::Normal { key } = note_ev.note.note_type {
             let real_sec = note_ev.real_time;
+            // 鍵盤番号を0始まりのインデックスに変換する
+            let idx = (key - KEY_FIRST_NUM) as usize;
             if self.initialized() {
                 self.first_time = real_sec;
-                // TODO: keyをusizeに変換するのでkeyがi32の意味がない. 鍵盤の使い方を検討
-                self.lane[key as usize] = true;
-            } else {
-                self.lane[key as usize] = true;
             }
+            self.lane[idx] = true;
         }
     }
 
